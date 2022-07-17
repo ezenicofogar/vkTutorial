746fcca057fd9954e91448887893cf087f99839e
ezenicofogar <107578772+ezenicofogar@users.noreply.github.com>
2022-07-17 18:06:49 -0300

    Base Code (app skeleton).

--- Application/source/Application.cpp
+++ Application/source/Application.cpp
@@ -1,37 +1,70 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+
+class TriangleApp
+{
+public:
+	void run();
+	static constexpr uint32_t windowWidth{ 1280U };
+	static constexpr uint32_t windowHeight{ 720U };
+private:
+	void initWindow();
+	void initVulkan();
+	void mainLoop();
+	void cleanup();
+	GLFWwindow* window;
+};
 
 int main(int argc, char* argv[])
+{
+	TriangleApp app;
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
+	return 0;
+}
+
+void TriangleApp::run()
+{
+	initVulkan();
+	mainLoop();
+	cleanup();
+}
+
+void TriangleApp::initWindow()
 {
 	glfwInit();
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	window = glfwCreateWindow(windowWidth, windowHeight, "Vulkan", nullptr, nullptr);
+}
 
-	std::cout << extensionCount << " extensions supported" << std::endl;
+void TriangleApp::initVulkan()
+{
 
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
+}
 
+void TriangleApp::mainLoop()
+{
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 	}
-	
+}
+
+void TriangleApp::cleanup()
+{
 	glfwDestroyWindow(window);
-
 	glfwTerminate();
-
-	return 0;
 }
